8efa5b21afbec68e77114ab0583df8a4ae82b8e8
Linc <test@testserver.com>
2018-01-09 12:46:23 -0600

    new file from a job interview

---
+++ recursiveList.py
@@ -0,0 +1,14 @@
+def listFromApi(self):
+    dataList = []
+    infoDictionary = {}
+    #
+    infoDictionary.setdefault('student1')
+    dataList.append()
+    return dataList
+
+def main():
+    #get data
+    listFromApi()
+    # return each element of the list recursively
+    i = len(listFromApi()) - 1
+    print()
